783ace117388e2810a3e4efa5977e192fffdeb63
Jun Sun <sunjun16sj@gmail.com>
2021-12-20 00:07:12 +0800

    Add files via upload

---
+++ aa/a2.py
@@ -0,0 +1,13 @@
+import numpy as np 
+
+#import torch
+#import matplotlib.pyplot as plt
+#b = torch.tensor([1,2,3])
+#b = np.array([1,2,3])
+#print(b)
+print('junsun')
+def ac(a, b):
+    return a * b
+
+c = ac(5, 6)
+print(c)
